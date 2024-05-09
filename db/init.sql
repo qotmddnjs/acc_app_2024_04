de4a1d6bbbf81182db8acc55ad16a8a3dcd82663
qotmddnjs <dudcnsaus@gmail.com>
2024-05-09 14:08:47 +0900

    RebateOrderItem에 구매자 정보, 정산 금액 환급 지급 내역 추가

--- db/init.sql
+++ db/init.sql
@@ -9,10 +9,10 @@ CREATE DATABASE acc_app_2024_04__dev;
 USE acc_app_2024_04__dev;
 
 
--- SELECT order_item_id, pay_price, refund_price, wholesale_price, pg_fee,
---        CASE
---            WHEN pay_price = refund_price
---                THEN 0
---            ELSE wholesale_price - pg_fee
---            END AS rebate_price
--- FROM `rebate_order_item`;
+SELECT order_item_id, pay_price, refund_price, wholesale_price, pg_fee,
+       CASE
+            WHEN pay_price = refund_price
+               THEN 0
+           ELSE wholesale_price - pg_fee
+           END AS rebate_price
+ FROM `rebate_order_item`;

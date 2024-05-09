03e367c4e65436fbe57bacf5b2c3d65e95849c18
qotmddnjs <dudcnsaus@gmail.com>
2024-05-09 13:16:32 +0900

    정산 데이터 생성

--- src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/controller/AdmRebateController.java
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/controller/AdmRebateController.java
@@ -1,16 +1,38 @@
 package com.koreait.exam.acc_app_2024_04.app.rebate.controller;
 
+import com.koreait.exam.acc_app_2024_04.app.rebate.service.RebateService;
+import com.koreait.exam.acc_app_2024_04.util.Ut;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/adm/rebate")
+@RequiredArgsConstructor
 public class AdmRebateController {
+
+  private final RebateService rebateService;
+
   @GetMapping("/makeData")
   @PreAuthorize("hasAuthority('ADMIN')")
-  public String showMakeData(){
+  public String showMakeData() {
     return "adm/rebate/makeData";
   }
+
+  @PostMapping("/makeData")
+  @PreAuthorize("hasAuthority('ADMIN')")
+  @ResponseBody
+  public String makeData(String yearMonth) {
+
+    rebateService.makeDate(yearMonth);
+
+    return "success";
+  }
+
 }
